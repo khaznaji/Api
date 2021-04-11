b61cb77a7dad10604cf0ee7a6bbfbd08cdd8e453
Khaznaji <oumaymalyna.khaznaji@esprit.tn>
2021-04-11 16:11:52 +0200

    api

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UserList from'./component/UserList';
+function App() {
+  return (
+    <div className="container">
+      <UserList/>
+    </div>
+  );
+}
+
+export default App;
